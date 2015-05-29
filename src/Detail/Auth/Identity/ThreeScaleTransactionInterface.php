af1e54a2c58b5965f8508235f55e0b519c36e68b
Ivan Wolf <iwolf@detailag.ch>
2015-05-29 14:59:03 +0200

    Defined getters in ThreeScaleTransaction interface

--- src/Detail/Auth/Identity/ThreeScaleTransactionInterface.php
+++ src/Detail/Auth/Identity/ThreeScaleTransactionInterface.php
@@ -2,6 +2,37 @@
 
 namespace Detail\Auth\Identity;
 
+use DateTime;
+
 interface ThreeScaleTransactionInterface
 {
+    /**
+     * @return string
+     */
+    public function getAppId();
+
+    /**
+     * @return DateTime
+     */
+    public function getReceivedOn();
+
+    /**
+     * @return array
+     */
+    public function getUsage();
+
+    /**
+     * @return string
+     */
+    public function getRequest();
+
+    /**
+     * @return string
+     */
+    public function getResponse();
+
+    /**
+     * @return int
+     */
+    public function getResponseCode();
 }
