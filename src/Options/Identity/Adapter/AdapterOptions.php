828a240e78e9d239ad06d85f81a818ccc888c24a
Ivan Wolf <iwolf@detailag.ch>
2018-06-12 16:55:24 +0200

    Ignoring __strictMode__ property for phpmd

--- src/Options/Identity/Adapter/AdapterOptions.php
+++ src/Options/Identity/Adapter/AdapterOptions.php
@@ -4,10 +4,15 @@ namespace Detail\Auth\Options\Identity\Adapter;
 
 use Zend\Stdlib\AbstractOptions;
 
+/**
+ * @SuppressWarnings(PHPMD.CamelCasePropertyName)
+ */
 class AdapterOptions extends AbstractOptions
 {
+    // @codingStandardsIgnoreStart
     /**
      * @var bool
      */
     protected $__strictMode__ = false;
+    // @codingStandardsIgnoreEnd
 }
